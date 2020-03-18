2013c0e73eb2758f3ec4932e5cdde47275327280
Sunny Liu <sunny.6.2012@gmail.com>
2020-03-17 23:18:09 -0700

    2nd commit

--- password_check.py
+++ password_check.py
@@ -1,13 +1,12 @@
-x = 0
-y = 1
-while x < 3 and y < 3:
- 	pass_word=input('請輸入密碼') 
- 	if str(pass_word) == 'a123456':
-		 print ('login successfully')
-		 break
- 	else:
-  		x = x + 1 
-  		y = 3 - x 
-  		
-  		print ('還有',y,'次機會')
-print ('已錯誤超過三次,登入失敗')
+i = 3
+while True:
+ pass_word=input('請輸入密碼') 
+ if str(pass_word) == 'a123456':
+  print ('login successfully')
+  break
+ else:
+  i = i -1  
+  print ('還有',i,'次機會')
+  if i == 0:
+   print ('已錯誤超過三次,登入失敗')
+   break
