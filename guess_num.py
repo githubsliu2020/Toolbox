52e9b9d217d0c98f88e0b55df5e3f17f40e41d76
Sunny Liu <sunny.6.2012@gmail.com>
2020-03-21 20:08:12 -0700

    first commit

---
+++ guess_num.py
@@ -0,0 +1,17 @@
+# 建立隨機整數1-100
+#讓使用者猜測數字
+#猜對就可以回答"答對了"
+#猜錯就提示答案比猜測的數字大或小
+
+import ramdom
+r = ramdom.randint(1,100)
+num = input('請猜出1~100隨機出碼的數字')
+num = int(num)
+while True:
+	if num == r
+	print ('好厲害!答對了!')
+	break
+	elif num > r:
+		print('您的數字比答案大')
+	elif num < r:
+		print('您的數字比答案小')
